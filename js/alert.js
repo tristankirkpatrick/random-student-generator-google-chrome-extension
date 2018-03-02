d41111dc2edd15d4d8a64cdef879d83a2302ea29
Tristan Kirkpatrick <tristankirkpatrick90@gmail.com>
2018-03-02 11:24:03 +0000

    Tidied alert code.

--- js/alert.js
+++
@@ -1,2 +0,0 @@
-
-"swal({title:&#39;" + selectedStudent[0] + "&#39;,html: &#39;" + selectedStudent[1] + "&#39;,timer:4000,onOpen:()=>{swal.showLoading()}}).then((result)=>{if(result.dismiss===&#39;timer&#39;){}})"
