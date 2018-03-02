16ebe2b950b35ac1670dbaec842698287f1ea589
Tristan Kirkpatrick <tristankirkpatrick90@gmail.com>
2018-03-02 11:03:46 +0000

    Moved Google Analytics code to beginning of the file.

--- popup.js
+++ popup.js
@@ -1,5 +1,15 @@
 window.onload = function controller() {
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-113130467-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
     $("#nostudents").hide();
     chrome.storage.local.get(['storedClassrooms'], function (courses) {
         $("#content").append("<div id='courseList'></div > ");
@@ -65,13 +75,4 @@ function loadStudents() {
         })
     })
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-113130467-2']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = 'https://ssl.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
 }
